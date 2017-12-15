d65acff6001a90b5e0dd8257f3d957e68354c13d
dnbaker <dnb@cs.jhu.edu>
2017-12-14 19:35:04 -0500

    Stuff.

--- test.cpp
+++ test.cpp
@@ -1,6 +1,8 @@
 #include <cstdlib>
 #include <cstdio>
 #include <chrono>
+#include <ctime>
+#include <random>
 #include <algorithm>
 #include <numeric>
 #include <chrono>
@@ -13,7 +15,7 @@ using namespace std::chrono;
 
 using tp = std::chrono::system_clock::time_point;
 
-static const size_t BITS = 25;
+static const size_t BITS = 18;
 
 
 bool test_qty(size_t lim) {
@@ -26,12 +28,15 @@ struct kt_data {
     hll::hll_t &hll_;
     const std::uint64_t n_;
     const int nt_;
+    const std::vector<std::uint64_t> &inputs_;
 };
 
 void kt_helper(void *data, long index, int tid) {
     hll::hll_t &hll(((kt_data *)data)->hll_);
+    const std::vector<std::uint64_t> &inputs(((kt_data *)data)->inputs_);
     const std::uint64_t todo((((kt_data *)data)->n_ + ((kt_data *)data)->nt_ - 1) / ((kt_data *)data)->nt_);
-    for(std::uint64_t i(index * todo), e(std::min(((kt_data *)data)->n_, (index + 1) * todo)); i < e; hll.addh(i++));
+    const std::uint64_t end(std::min(((kt_data *)data)->n_, (index + 1) * todo));
+    for(std::uint64_t i(index * todo); i < end; hll.addh(inputs[i++]));
 }
 
 void usage() {
@@ -55,7 +60,7 @@ void estimate(hll::hll_t &h1, hll::hll_t &h2, hll::dhll_t &h3, std::uint64_t exp
 int main(int argc, char *argv[]) {
     if(argc < 2) usage();
     using clock_t = std::chrono::system_clock;
-    unsigned nt(8), pb(1 << 18);
+    unsigned nt(8), pb(1 << 15);
     double eps(1e-10);
     std::vector<std::uint64_t> vals;
     int c;
@@ -70,16 +75,24 @@ int main(int argc, char *argv[]) {
     for(c = optind; c < argc; ++c) vals.push_back(strtoull(argv[c], 0, 10));
     if(vals.empty()) vals.push_back(1ull<<(BITS+1));
     std::fprintf(stderr, "#Label\th1\th2\th3\n");
+    std::vector<std::uint64_t> inputs;
+    std::mt19937_64 gen(std::time(nullptr));
+    if(vals.size() == 1) {
+        std::uniform_int_distribution<uint64_t> dist(0, 1ull << 32ull);
+        while(vals.size() < 64) vals.push_back(dist(gen));
+    }
     for(const auto val: vals) {
         std::fprintf(stderr, "#Value = %" PRIu64 "\n", val);
         hll::hll_t t(BITS), t2(BITS + 1);
         hll::dhll_t t3(BITS);
+        inputs.resize(val);
+        for(auto &el: inputs) el = gen();
 #ifndef THREADSAFE
-        for(size_t i(0); i < val; t.addh(i), t2.addh(i), t3.addh(i), ++i);
+        for(const auto el: inputs) t.addh(el), t2.addh(el), t3.addh(el);
 #else
-        kt_data data {t, val, (int)nt};
-        kt_data data2{t2, val, (int)nt};
-        kt_data data3{t3, val, (int)nt};
+        kt_data data {t, val, (int)nt, inputs};
+        kt_data data2{t2, val, (int)nt, inputs};
+        kt_data data3{t3, val, (int)nt, inputs};
         kt_for(nt, &kt_helper, &data, (val + nt - 1) / nt);
         kt_for(nt, &kt_helper, &data2, (val + nt - 1) / nt);
         kt_for(nt, &kt_helper, &data3, (val + nt - 1) / nt);
