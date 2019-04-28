43ee5f32de3c788c9392e8d05ba037eaf56e5e53
dnbaker <dnb@cs.jhu.edu>
2019-04-28 09:34:56 -0400

    Benchmark sparse versions. Push back/sort isn't looking bad.

--- src/testsparse.cpp
+++ src/testsparse.cpp
@@ -26,19 +26,31 @@ int main() {
     auto rshift = 64 - h1.p(), lshift = h1.p();
     std::map<uint32_t, uint8_t> tmp;
     std::vector<uint32_t> i1, i2;
-    for(size_t i = 0; i < 150; ++i) {
+    Timer zomg("");
+    uint64_t zomgsum = 0, vtimesum = 0;
+    enum {FIRST_LOOP_N = 1000};
+    for(size_t i = 0; i < FIRST_LOOP_N; ++i) {
         const auto v = h1.hash(i);
         auto ind = v >> rshift;
         auto val = uint8_t(clz(v << lshift) + 1);
         auto encval = SparseHLL32::encode_value(ind, val);
         assert(SparseHLL32::get_index(SparseHLL32::encode_value(ind, uint8_t(val))) == (ind));
         assert(SparseHLL32::get_value(SparseHLL32::encode_value(ind, uint8_t(val))) == uint8_t(val));
+        zomg.restart()
         tmp[ind] = std::max(tmp[ind], uint8_t(val));
+        zomg.stop();
+        zomgsum += zomg.report();
         assert(ind == SparseHLL32::get_index(encval));
         assert((encval >> 6) == ind);
+        zomg.restart()
         i1.push_back(encval);
+        zomg.stop();
+        vtimesum += zomg.report();
         h1.addh(i);
     }
+    vtimesum /= FIRST_LOOP_N;
+    zomgsum /= FIRST_LOOP_N;
+    std::fprintf(stderr, "Runtime for updating tmp variable: %zu\n", size_t(zomgsum) / 150);
     for(size_t i = 140; i < 240; ++i) {
         const auto v = h2.hash(i);
         auto ind = v >> rshift;
@@ -61,17 +73,23 @@ int main() {
     flatten(i1copy);
     double single_flattentime, single_maptime;
     std::array<double, 3> flattened_vals;
+    zomgsum = 0;
     {
         Timer t("zomg");
         flattened_vals = flatten_and_query(i1copy, h2, nullptr, true);
         t.stop();
         single_flattentime = t.report();
+        t.restart();
+        auto vals = pair_query(tmp, h1);
+        t.stop();
         std::fprintf(stderr, "Single flatten and query, including sort time %zu\n", size_t(t.report()));
+        std::fprintf(stderr, "vtime each: %zu. zomgtime: %zu. Bulk processing for vector %zu. Total score for vector approach: %zu. Total score for other approach: %zu\n",
+                              size_t(vtimesum), size_t(zomgsum), single_flattentime, vtimesum * FIRST_LOOP_N + single_flattentime, zomg + t.report());
         t.restart();
         auto ovals = h.jaccard_index(h2);
         for(size_t i = 99; i--;ovals = h.jaccard_index(h2));
         t.stop();
-        std::fprintf(stderr, "Single dense HLL query %zu\n", size_t(t.report()));
+        std::fprintf(stderr, "Single dense HLL query %zu\n", size_t(t.report()) / 100);
     }
     {
         Timer t("zomg");
@@ -90,7 +108,7 @@ int main() {
     t.stop();
     std::fprintf(stderr, "Time for pair query with map: %zu\n", size_t(t.report()));
     single_maptime = t.report();
-    std::fprintf(stderr, "map is %lf as slow as vector\n", double(single_flattentime) / double(single_maptime));
+    std::fprintf(stderr, "map is %lf as fast as vector\n", double(single_flattentime) / double(single_maptime));
     auto it = tmp.begin();
     size_t ind = 0;
     i1copy = i1;
