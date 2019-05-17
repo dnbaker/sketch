3307112fae9662af368117320e888f6f8fe2e8c2
dnbh <dnb@cs.jhu.edu>
2019-05-17 13:03:17 -0400

    Update

--- parmerge.h
+++ parmerge.h
@@ -8,6 +8,7 @@
 #define MERGE_SORTED_PARALLEL_CPU_H__
 namespace sketch {
 // https://www.cc.gatech.edu/sites/default/files/images/fast_and_adaptive_list_intersections_on_the_gpu.pdf
+// http://www.adms-conf.org/p1-SCHLEGEL.pdf
 }
 
 #endif /* MERGE_SORTED_PARALLEL_CPU_H__ */
