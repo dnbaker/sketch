dc87c6f47ba63b349ad561038df2ccaeee92e6c7
dnbaker <dnb@cs.jhu.edu>
2019-07-08 01:04:22 -0400

    Correctly check cplusplus standard for support of if constexpr.

--- common.h
+++ common.h
@@ -79,11 +79,11 @@
 #endif
 
 #ifndef CONST_IF
-#if __cplusplus >= 201703L
-#define CONST_IF(...) if constexpr(__VA_ARGS__)
-#else
-#define CONST_IF(...) if(__VA_ARGS__)
-#endif
+#  if __cplusplus >= __cpp_if_constexpr
+#    define CONST_IF(...) if constexpr(__VA_ARGS__)
+#  else
+#    define CONST_IF(...) if(__VA_ARGS__)
+#  endif
 #endif
 
 
