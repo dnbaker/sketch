e05c2b071f8b726b75a8b83d70a4bf22f997f41d
Daniel Baker <daniel.n.baker@ibm.com>
2019-07-26 14:19:02 -0700

    Simplify containment.

--- hll.h
+++ hll.h
@@ -1145,15 +1145,8 @@ public:
         return const_cast<hllbase_t &>(*this).jaccard_index(const_cast<const hllbase_t &>(h2));
     }
     double containment_index(const hllbase_t &h2) const {
-        if(jestim_ == JointEstimationMethod::ERTL_JOINT_MLE) {
-            auto full_cmps = ertl_joint(*this, h2);
-            const auto ret = full_cmps[2] / (full_cmps[0] + full_cmps[2]);
-            return ret;
-        }
-        const double us = union_size(h2);
-        const double my_sz = creport();
-        const double ret = (my_sz + h2.creport() - us) / my_sz;
-        return std::max(0., ret);
+        auto fsr = full_set_comparison(h2);
+        return fsr[2] / (fsr[2] + fsr[0]);
     }
     std::array<double, 3> full_set_comparison(const hllbase_t &h2) const {
         if(jestim_ == JointEstimationMethod::ERTL_JOINT_MLE) {
