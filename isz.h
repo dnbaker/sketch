d41bb34a43f957a729b05c73d89f58ff5770a16a
dnbaker <dnb@cs.jhu.edu>
2019-09-09 09:50:13 -0400

    Better branch prediction in intersection_size.

--- isz.h
+++ isz.h
@@ -7,21 +7,20 @@ namespace common {
 template<typename Container, typename Cmp=typename Container::key_compare>
 std::uint64_t intersection_size(const Container &c1, const Container &c2, const Cmp &cmp=Cmp()) {
     // These containers must be sorted.
+    auto it1 = std::begin(c1);
+    auto it2 = std::begin(c2);
+    const auto e1 = std::cend(c1);
+    const auto e2 = std::cend(c2);
+    if(it1 == e1 || it2 == e2) return 0;
     std::uint64_t ret = 0;
-    auto it1 = c1.begin();
-    auto it2 = c2.begin();
-    const auto e1 = c1.end();
-    const auto e2 = c2.end();
-    if(it1 != e1 && it2 != e2) {
-        for(;;) {
-            if(cmp(*it1, *it2)) {
-                if(++it1 == e1) break;
-            } else if(cmp(*it2, *it1)) {
-                if(++it2 == e2) break;
-            } else {
-                ++ret;
-                if(++it1 == e1 || ++it2 == e2) break;
-            }
+    FOREVER {
+        if(*it1 == *it2) { // Easily predicted
+            ++ret;
+            if(++it1 == e1 || ++it2 == e2) break;
+        } else if(cmp(*it1, *it2)) {
+            if(++it1 == e1) break;
+        } else {
+            if(++it2 == e2) break;
         }
     }
     return ret;
