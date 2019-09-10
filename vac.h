bd1d7d12b2d7d5ec03efe5932e19e87dce260e89
dnbh <dnb@cs.jhu.edu>
2019-09-10 12:05:39 -0400

    Simplify.

--- vac.h
+++ vac.h
@@ -14,7 +14,12 @@ template<typename BaseSketch,
          typename...VectorArgs>
 struct VACSketch {
     using base = BaseSketch;
+
+    // Members
     Container<base, VectorArgs...> sketches_;
+    const unsigned n_;
+
+    // For seeding each thread's generator separately
     struct ThreadSeededGen: public RNG {
         template<typename...Args>
         ThreadSeededGen(Args &&...args): RNG(std::forward<Args>(args)...) {
@@ -25,19 +30,22 @@ struct VACSketch {
         template<typename...Args>
         decltype(auto) operator()(Args &&...args) const {return RNG::operator()(std::forward<Args>(args)...);}
     };
-    const unsigned n_;
+
+    // Construction
     template<typename...Args>
     VACSketch(size_t n, Args &&...args): n_(n) {
         if(n <= 1) throw std::runtime_error(std::string(__PRETTY_FUNCTION__) + " requires n >= 2");
         sketches_.reserve(n);
         for(size_t i = n; i--; sketches_.emplace_back(std::forward<Args>(args)...));
     }
+    // Addition
     void addh(uint64_t x) {
         thread_local ThreadSeededGen gen;
         const auto end = std::min(ctz(~gen()) + 1, n_);
         unsigned i = 0;
         do sketches_[i++].addh(x); while(i < end);
     }
+    // Composition
     VACSketch &operator+=(const VACSketch &o) {
         if(n_ != o.n_) throw std::runtime_error("Mismatched vacsketch counts");
         for(auto i1 = sketches_.begin(), i2 = o.sketches_.begin(), e1 = sketches_.end();
