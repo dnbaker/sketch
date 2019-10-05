36fa0f4cb5d06031ef31102a7112e5c95f83a365
dnbh <dnb@cs.jhu.edu>
2019-10-05 09:49:13 -0400

    Linking.

--- include/exception.h
+++ include/exception.h
@@ -35,20 +35,23 @@ public:
 };
 
 class ZlibError: public std::runtime_error {
-    static constexpr const char * const z_errmsg[10] = {
-        (z_const char *)"need dictionary",     /* Z_NEED_DICT       2  */
-        (z_const char *)"stream end",          /* Z_STREAM_END      1  */
-        (z_const char *)"",                    /* Z_OK              0  */
-        (z_const char *)"file error",          /* Z_ERRNO         (-1) */
-        (z_const char *)"stream error",        /* Z_STREAM_ERROR  (-2) */
-        (z_const char *)"data error",          /* Z_DATA_ERROR    (-3) */
-        (z_const char *)"insufficient memory", /* Z_MEM_ERROR     (-4) */
-        (z_const char *)"buffer error",        /* Z_BUF_ERROR     (-5) */
-        (z_const char *)"incompatible version",/* Z_VERSION_ERROR (-6) */
-        (z_const char *)""
-    };
+    static const char *es(int c) {
+        static constexpr const char * const z_errmsg[10] = {
+            (z_const char *)"need dictionary",     /* Z_NEED_DICT       2  */
+            (z_const char *)"stream end",          /* Z_STREAM_END      1  */
+            (z_const char *)"",                    /* Z_OK              0  */
+            (z_const char *)"file error",          /* Z_ERRNO         (-1) */
+            (z_const char *)"stream error",        /* Z_STREAM_ERROR  (-2) */
+            (z_const char *)"data error",          /* Z_DATA_ERROR    (-3) */
+            (z_const char *)"insufficient memory", /* Z_MEM_ERROR     (-4) */
+            (z_const char *)"buffer error",        /* Z_BUF_ERROR     (-5) */
+            (z_const char *)"incompatible version",/* Z_VERSION_ERROR (-6) */
+            (z_const char *)""
+        };
+        return z_errmsg[Z_NEED_DICT - c];
+    }
 public:
-    ZlibError(int ze, std::string s): std::runtime_error(std::string("zlibError [") + z_errmsg[Z_NEED_DICT-ze] + "]" + s) {}
+    ZlibError(int ze, std::string s): std::runtime_error(std::string("zlibError [") + es(ze) + "]" + s) {}
 };
 
 #ifdef __CUDACC__
