37b621e0ab8f643f2835e69df4dcfb8454c27aa2
dnbaker <dnb@cs.jhu.edu>
2019-10-20 22:34:09 -0400

    Count sketch compression complete.

--- include/cs_compress.h
+++ include/cs_compress.h
@@ -12,26 +12,29 @@
 
 namespace sketch {
 
+// TODO: write a generic method compatible with pytorch/ATen
+
 template<typename C>
 auto cs_compress(const C &in, size_t newdim, const KWiseHasherSet<4> &hf) {
+    using FT = std::decay_t<decltype(*std::begin(in))>;
     if(newdim > in.size()) throw 1;
     const size_t ns = hf.size();
     schism::Schismatic<uint32_t> div(newdim);
-    using FT = std::decay_t<decltype(*std::begin(in))>;
     std::vector<FT> ret(newdim * ns);
     for(unsigned j = 0; j < ns; ++j) {
         for(size_t i = 0; i < in.size(); ++i) {
             const auto v = in[i];
             auto hv = hf(i, j);
-            ret[div.mod(hv >> 1) + in.size() * j] += v * (hv & 1 ? 1: -1);
+            auto ind = div.mod(hv >> 1) * ns + j;
+            ret.at(ind) += v * (hv & 1 ? 1: -1);
         }
     }
     return ret;
 }
 template<typename C>
 auto cs_decompress(const C &in, size_t newdim, size_t olddim, const KWiseHasherSet<4> &hf) {
-    if(newdim < in.size()) throw 1;
     const size_t ns = hf.size();
+    //if(newdim < in.size())
     schism::Schismatic<uint32_t> div(olddim);
     using FT = std::decay_t<decltype(*std::begin(in))>;
     std::vector<FT> ret(newdim);
@@ -50,8 +53,8 @@ auto cs_decompress(const C &in, size_t newdim, size_t olddim, const KWiseHasherS
 }
 
 template<typename C>
-std::vector<unsigned> top_indices_from_compressed(const C &in, size_t newdim, size_t olddim, const KWiseHasherSet<4> &hf, unsigned k) {
-    if(newdim < in.size()) throw 1;
+auto top_indices_from_compressed(const C &in, size_t newdim, size_t olddim, const KWiseHasherSet<4> &hf, unsigned k) {
+    //if(newdim < in.size()) throw 1;
     const size_t ns = hf.size();
     schism::Schismatic<uint32_t> div(olddim);
     using FT = std::decay_t<decltype(*std::begin(in))>;
@@ -62,18 +65,25 @@ std::vector<unsigned> top_indices_from_compressed(const C &in, size_t newdim, si
         auto tmp = mem.get();
         for(unsigned j = 0; j < ns; ++j) {
             auto hv = hf(i, j);
-            tmp[j] = in[div.mod(hv >> 1) * ns + j] * (hv & 1 ? 1: -1);
+            tmp[j] = in.at(div.mod(hv >> 1) * ns + j) * (hv & 1 ? 1: -1);
         }
         std::sort(tmp, tmp + hf.size());
+        std::pair<FT, unsigned> pair = std::make_pair((tmp[ns >> 1] + tmp[(ns - 1) >> 1]) * .5, unsigned(i));
         OMP_PRAGMA("omp critical")
         {
-            pq.push(std::make_pair((tmp[ns >> 1] + tmp[(ns - 1) >> 1]) * .5, i));
+            pq.push(pair);
             if(pq.size() > k) pq.pop();
         }
     }
-    std::vector<std::pair<FT, unsigned>> ret;
-    ret.reserve(k);
-    for(unsigned i = 0; i < k; ret[i++] = pq.top(), pq.pop());
+    std::pair<std::vector<FT>, std::vector<unsigned>> ret;
+    ret.first.reserve(k);
+    ret.second.reserve(k);
+    for(unsigned i = 0; i < k; ++i) {
+        const auto &r(pq.top());
+        ret.first.push_back(r.first);
+        ret.second.push_back(r.second);
+        pq.pop();
+    }
     return ret;
 }
 
---
+++ src/cscomp.cpp
@@ -0,0 +1,16 @@
+#include "cs_compress.h"
+
+
+int main() {
+    sketch::KWiseHasherSet<4> hf(1337);
+    std::vector<double> init(1000, 1.);
+    for(size_t i = 0; i < init.size(); ++i) init[i] = std::pow(i, 2);
+    auto step1 = cs_compress(init, 100, hf);
+    auto step2 = cs_decompress(step1, 1000, 100, hf);
+    // top_indices_from_compressed(const C &in, size_t newdim, size_t olddim, const KWiseHasherSet<4> &hf, unsigned k) 
+    auto topind = top_indices_from_compressed(step1, 1000, 100, hf, 20);
+    std::fprintf(stderr, "topind sizes: %zu, %zu\n", topind.first.size(), topind.second.size());
+    for(const auto i: topind.first) {
+        std::fprintf(stderr, "wooo %lf\n", double(i));
+    }
+}
