62a8758eb03b55c856276fc7ccf3c6708e4569f4
dnbaker <dnb@cs.jhu.edu>
2020-12-09 11:23:15 -0500

    Replace z_const with const in ZlibError

--- include/sketch/exception.h
+++ include/sketch/exception.h
@@ -64,16 +64,16 @@ static int postcondition_require(bool condition, std::string s, int ec=0) {
 class ZlibError: public std::runtime_error {
     static const char *es(int c) {
         static constexpr const char * const z_errmsg[10] = {
-            (z_const char *)"need dictionary",     /* Z_NEED_DICT       2  */
-            (z_const char *)"stream end",          /* Z_STREAM_END      1  */
-            (z_const char *)"",                    /* Z_OK              0  */
-            (z_const char *)"file error",          /* Z_ERRNO         (-1) */
-            (z_const char *)"stream error",        /* Z_STREAM_ERROR  (-2) */
-            (z_const char *)"data error",          /* Z_DATA_ERROR    (-3) */
-            (z_const char *)"insufficient memory", /* Z_MEM_ERROR     (-4) */
-            (z_const char *)"buffer error",        /* Z_BUF_ERROR     (-5) */
-            (z_const char *)"incompatible version",/* Z_VERSION_ERROR (-6) */
-            (z_const char *)""
+            (const char *)"need dictionary",     /* Z_NEED_DICT       2  */
+            (const char *)"stream end",          /* Z_STREAM_END      1  */
+            (const char *)"",                    /* Z_OK              0  */
+            (const char *)"file error",          /* Z_ERRNO         (-1) */
+            (const char *)"stream error",        /* Z_STREAM_ERROR  (-2) */
+            (const char *)"data error",          /* Z_DATA_ERROR    (-3) */
+            (const char *)"insufficient memory", /* Z_MEM_ERROR     (-4) */
+            (const char *)"buffer error",        /* Z_BUF_ERROR     (-5) */
+            (const char *)"incompatible version",/* Z_VERSION_ERROR (-6) */
+            (const char *)""
         };
         return z_errmsg[Z_NEED_DICT - c];
     }
