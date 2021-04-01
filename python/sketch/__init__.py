794465926b5e5ecaccaeb93575f80286774e3c41
dnbh <dnb@cs.jhu.edu>
2021-04-01 15:41:37 -0400

    Add docs

--- python/sketch/__init__.py
+++ python/sketch/__init__.py
@@ -9,6 +9,15 @@ from collections import namedtuple
 SetSketchParams = namedtuple("SetSketchParams", 'a, b')
 
 def optimal_ab(maxv, minv, *, q):
+    '''
+        Calculate a and b for maxv and minv, such that the maxv is mapped to
+        0 and minv's value is mapped to q.
+        :param maxv: float value which is the maximum to be quantized
+        :param minv: float value which is the minimum to be quantized
+        :param q:    float or integral value for the ceiling; required.
+        :return: namedtuple SetSketchParams, consisting of (a, b); access through ssp.a, ssp[0], or tuple access
+    '''
+
     if maxv < minv:
         minv, maxv = maxv, minv
     from numpy import exp as nexp, log as nlog
