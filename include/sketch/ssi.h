12c4c3961614ca5f9d88fb0c92768c6eb83fcfca
dnbh <dnb@cs.jhu.edu>
2021-04-26 09:07:40 -0400

    Add update_query, which constructs and queries the structure simultaneously

--- include/sketch/ssi.h
+++ include/sketch/ssi.h
@@ -41,12 +41,12 @@ private:
     using HashV = std::vector<HashMap>;
     std::vector<HashV> packed_maps_;
     std::vector<uint64_t> regs_per_reg_;
-    size_t total_ids_ = 0;
+    std::atomic<size_t> total_ids_;
 public:
     using key_type = KeyT;
     using id_type = IdT;
     size_t m() const {return m_;}
-    size_t size() const {return total_ids_;}
+    size_t size() const {return total_ids_.load();}
     size_t ntables() const {return packed_maps_.size();}
     template<typename IT, typename Alloc, typename OIT, typename OAlloc>
     SetSketchIndex(size_t m, const std::vector<IT, Alloc> &nperhashes, const std::vector<OIT, OAlloc> &nperrows): m_(m) {
@@ -59,9 +59,11 @@ public:
             if(v2 <= 0) v2 = v1;
             packed_maps_.emplace_back(v2);
         }
+        total_ids_.store(0);
     }
     template<typename IT, typename Alloc>
     SetSketchIndex(size_t m, const std::vector<IT, Alloc> &nperhashes): m_(m) {
+        total_ids_.store(0);
         for(const auto v: nperhashes) {
             if(size_t(v) > m) throw std::invalid_argument("Cannot create LSH keys with v > m");
             regs_per_reg_.push_back(v);
@@ -69,6 +71,7 @@ public:
         }
     }
     SetSketchIndex(size_t m, bool densified=false): m_(m) {
+        total_ids_.store(0);
         uint64_t rpr = 1;
         const size_t nrpr = densified ? m: size_t(ilog2(sketch::integral::roundup(m)));
         regs_per_reg_.reserve(nrpr);
@@ -84,9 +87,44 @@ public:
         }
     }
     template<typename Sketch>
+    std::tuple<std::vector<IdT>, std::vector<uint32_t>, std::vector<uint32_t>>
+    update_query(const Sketch &item, size_t maxcand, size_t starting_idx = size_t(-1)) {
+        if(item.size() < m_) throw std::invalid_argument(std::string("Item has wrong size: ") + std::to_string(item.size()) + ", expected" + std::to_string(m_));
+        if(starting_idx == size_t(-1) || starting_idx > regs_per_reg_.size()) starting_idx = regs_per_reg_.size();
+        const size_t my_id = std::atomic_fetch_add(&total_ids_, size_t(1));
+        const size_t n_subtable_lists = regs_per_reg_.size();
+        ska::flat_hash_map<IdT, uint32_t> rset;
+        std::vector<IdT> passing_ids;
+        std::vector<uint32_t> items_per_row;
+        rset.reserve(maxcand); passing_ids.reserve(maxcand); items_per_row.reserve(starting_idx);
+        for(size_t i = 0; i < n_subtable_lists; ++i) {
+            auto &subtab = packed_maps_[i];
+            const size_t nsubs = packed_maps_[i].size();
+            for(size_t j = 0; j < nsubs; ++j) {
+                auto &table = subtab[j];
+                KeyT myhash = hash_index(item, i, j);
+                auto it = table.find(myhash);
+                if(it == table.end()) table.emplace(myhash, {my_id});
+                else {
+                    for(const auto id: it->second) {
+                        auto rit2 = rset.find(id);
+                        if(rit2 == rset.end()) {
+                            rset.emplace(id, 1);
+                            passing_ids.push_back(id);
+                        } else ++rit2->second;
+                    }
+                    it->second.emplace_back(my_id);
+                }
+            }
+        }
+        std::vector<uint32_t> passing_counts(passing_ids.size());
+        std::transform(passing_ids.begin(), passing_ids.end(), passing_counts.begin(), [&rset](auto x) {return rset[x];});
+        return std::make_tuple(passing_ids, passing_counts, items_per_row);
+    }
+    template<typename Sketch>
     void update(const Sketch &item) {
-        if(item.size() != m_) throw std::invalid_argument(std::string("Item has wrong size: ") + std::to_string(item.size()) + ", expected" + std::to_string(m_));
-        const auto my_id = total_ids_++;
+        if(item.size() < m_) throw std::invalid_argument(std::string("Item has wrong size: ") + std::to_string(item.size()) + ", expected" + std::to_string(m_));
+        const size_t my_id = std::atomic_fetch_add(&total_ids_, size_t(1));
         const size_t n_subtable_lists = regs_per_reg_.size();
         for(size_t i = 0; i < n_subtable_lists; ++i) {
             auto &subtab = packed_maps_[i];
@@ -113,9 +151,9 @@ public:
         return XXH64_digest(&state);
     }
     template<typename Sketch>
-    std::pair<std::vector<IdT>, std::vector<uint32_t>>
+    std::tuple<std::vector<IdT>, std::vector<uint32_t>, std::vector<uint32_t>>
     query_candidates(const Sketch &item, size_t maxcand, size_t starting_idx = size_t(-1)) const {
-        if(starting_idx == size_t(-1)) starting_idx = regs_per_reg_.size();
+        if(starting_idx == size_t(-1) || starting_idx > regs_per_reg_.size()) starting_idx = regs_per_reg_.size();
         /*
          *  Returns ids matching input minhash sketches, in order from most specific/least sensitive
          *  to least specific/most sensitive
@@ -144,7 +182,9 @@ public:
             items_per_row.push_back(passing_ids.size() - items_before);
             if(rset.size() >= maxcand) break;
         }
-        return std::make_pair(passing_ids, items_per_row);
+        std::vector<uint32_t> passing_counts(passing_ids.size());
+        std::transform(passing_ids.begin(), passing_ids.end(), passing_counts.begin(), [&rset](auto x) {return rset[x];});
+        return std::make_tuple(passing_ids, passing_counts, items_per_row);
     }
 };
 
