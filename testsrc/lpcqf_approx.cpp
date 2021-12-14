74d8d72e555e1d9bf5822d05fef6c2b59335353b
Daniel N. Baker <dnb@cs.jhu.edu>
2021-12-14 12:27:43 -0500

    test wider radix sizes. return floating-point count estimates for approximate log case. Cache powers for LPCQF with approximate counting. Hardware intrinsics for PH/PS conversion

--- testsrc/lpcqf_approx.cpp
+++ testsrc/lpcqf_approx.cpp
@@ -3,11 +3,11 @@
 int main() {
     size_t nentered = 6;
     size_t ss = 32;
-    for(const auto v: {25, 1}) {
+    for(const auto v: {500, 25}) {
         sketch::LPCQF<uint16_t, 2, sketch::IS_APPROXINC | sketch::IS_QUADRATIC_PROBING | sketch::IS_POW2> lp(ss);
         for(size_t i = 0; i < nentered; ++i) {
             lp.update(i, v);
-            std::fprintf(stderr, "Value %d estimated as count %u\n", v, int(lp.count_estimate(i)));
+            std::fprintf(stderr, "Value %d estimated as count %Lg\n", v, (lp.count_estimate(i)));
             std::fprintf(stderr, "Inserted %zu items\n", i + 1);
         }
     }
@@ -15,8 +15,26 @@ int main() {
         sketch::LPCQF<uint16_t, 2, sketch::IS_APPROXINC | sketch::IS_QUADRATIC_PROBING | sketch::IS_POW2, 52, 50> lp(ss);
         for(size_t i = 0; i < nentered; ++i) {
             lp.update(i, 25);
-            std::fprintf(stderr, "Value 25 with base %Lg estimated as count %u\n", lp.approxlogb, int(lp.count_estimate(i)));
+            std::fprintf(stderr, "Value 25 with base %Lg estimated as count %Lg\n", lp.approxlogb, (lp.count_estimate(i)));
             std::fprintf(stderr, "Inserted %zu items\n", i + 1);
         }
     }
+    {
+        sketch::LPCQF<uint16_t, 2, sketch::IS_APPROXINC | sketch::IS_QUADRATIC_PROBING | sketch::IS_POW2, 10003, 10000> lp(ss);
+        for(size_t i = 0; i < nentered; ++i) {
+            lp.update(i, 25);
+            std::fprintf(stderr, "Value 25 with base %Lg estimated as count %Lg\n", lp.approxlogb, (lp.count_estimate(i)));
+            std::fprintf(stderr, "Inserted %zu items\n", i + 1);
+        }
+    }
+#if 0
+    {
+        sketch::LPCQF<uint16_t, 2, sketch::IS_APPROXINC | sketch::IS_QUADRATIC_PROBING | sketch::IS_POW2, 101, 3> lp(ss);
+        for(size_t i = 0; i < nentered; ++i) {
+            lp.update(i, 25);
+            std::fprintf(stderr, "Value 25 with base %Lg estimated as count %Lg\n", lp.approxlogb, (lp.count_estimate(i)));
+            std::fprintf(stderr, "Inserted %zu items\n", i + 1);
+        }
+    }
+#endif
 }
